230064d6913c763a4394f139ecbe8d90643861c5
Valentina <vjss05@gmail.com>
2024-06-25 20:09:03 +0200

    initial commit

--- learn-portal/src/App.tsx
+++ learn-portal/src/App.tsx
@@ -21,8 +21,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          holis <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
